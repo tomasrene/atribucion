11120ef13a3639435f8a6054630e8eb0c33ebba8
Tomas Reneboldi <tomasrene@gmail.com>
2021-12-01 23:27:22 +0100

    Deploy v1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='atribucion',
-    version='1.2',
+    version='1.3',
     description='Libreria para correr modelos de atribucion para marketing',
     long_description=long_description,
     long_description_content_type="text/markdown",
