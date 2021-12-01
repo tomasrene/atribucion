37e42d0a59b25e7410f43719183e582f304aa6f7
Tomas Reneboldi <tomasrene@gmail.com>
2021-12-01 23:16:20 +0100

    Version 1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='atribucion',
-    version='1.1',
+    version='1.2',
     description='Libreria para correr modelos de atribucion para marketing',
     long_description=long_description,
     long_description_content_type="text/markdown",
